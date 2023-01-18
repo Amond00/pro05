ac7438f2d98c8fe9f51de7a06805a1d289910d63
Amond00 <danielhan07@naver.com>
2023-01-18 18:24:13 +0900

    lg

---
+++ src/main/java/com/shop/myapp/config/MvcConfiguration.java
@@ -0,0 +1,26 @@
+package com.shop.myapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry re) {
+		/* '/img/**'로 호출하는 자원은 '/static/img/' 폴더 아래에서 찾는다. */
+		re.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+		/* '/video/**'로 호출하는 자원은 '/static/video/' 폴더 아래에서 찾는다. */
+		re.addResourceHandler("/video/**").addResourceLocations("classpath:/static/video/");	
+		/* '/css/**'로 호출하는 자원은 '/static/css/' 폴더 아래에서 찾는다. */
+		re.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+		/* '/js/**'로 호출하는 자원은 '/static/js/' 폴더 아래에서 찾는다. */
+		re.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+		/* '/css/**'로 호출하는 자원은 '/static/css/' 폴더 아래에서 찾는다. */
+		re.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+	
+	}
+}
